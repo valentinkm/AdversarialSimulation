35aaf6dfb495a8ca4664366338c7cf6203cf6b34
Leonard Kosanke <124199925+lkosanke@users.noreply.github.com>
2024-05-29 13:26:14 +0200

    IMPORTANT notes

--- LK/run_simulations.R
+++ LK/run_simulations.R
@@ -14,17 +14,39 @@ for (library_name in libraries) {
 r_scripts <- c("simulation1.R", "simulation1b.R")
 
 # Set up processing
+##Terminal innerhalb tardis:
+##tmux
+## srun -c40 --mem 30gb --pty --time 7-0:0:0 --partition long /bin/bash      #zum testen: --time 24:0:0 --partition test austauschen
+###(strg c bricht ab)
+
+##Warten bis Job fertig ist, dann Sprung in shell, da einfach, wenn ohne Docker image: 
+#Rscipt run_simulations.R
+
+#Mit docker, wieder im terminal
+#ins directory rein navigaten
+
+#apptainer shell datei.sif
+#Dann Rscript run_simulations.R
+
+###############falls connection weg ist:
+#Strg b+d und ein anderer noch zum hin und her springen
+#tmux 
+#########################
+
+##alles zusammen in einem, geht auch!!!!!
+##im Terminal, im login node:
+##in den LK Ordner rein mit cd
+##sbatch -c32 --mem 100gb --wrap "apptainer exec datei.sif Rscript run_simulations.R"
+
+##es erscheint über ls eine file, die den terminal output anzeigt (library output) mit:
+###cat filename (slurm...)
+
+##Am Ende downloaden immer einfach die ERgebnisfiles in Tardis. Ich kann sobald es läuft alles schließen!
+
+
+
 plan(multisession, workers = parallel::detectCores())
 
-#tweak(batchtools_slurm)
-#plan(tweak(batchtools_slurm,
-#           workers = 100,
-#           template = "/home/kosanke/AdversarialSimulation/LK/.batchtools.slurm.singularity.tmpl",
-#           resources=list(ncpus=1,
-#                          memore='700m',
-#                          walltime=6600,
-#                          partition=c('gpu'))
-#           ))
 
 # Function to source a script
 run_script <- function(script) {
