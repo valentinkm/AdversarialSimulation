154ac1d30864e3b1b2e1f146ee5483f8d020cb2a
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2024-06-05 14:20:14 +0200

    make sure results directory exists

--- VK/simulation/setup.R
+++ VK/simulation/setup.R
@@ -10,28 +10,18 @@ library(lavaan)
 library(purrr)
 library(parallel)
 library(Matrix)
-
-# check if running on Tardis cluster
-is_on_tardis <- function() {
-  grepl("tardis", Sys.info()["nodename"])
-}
+library(future.batchtools)
 
 # Set up the environment
 RNGkind("L'Ecuyer-CMRG")
 
-if (is_on_tardis()) {
-  library(future.batchtools)
-  plan(list(
-    #tweak(cluster, workers="tardis.mpib-berlin.mpg.de"),
-    tweak(batchtools_slurm,
-          # workers = 1, # not necessary
-          template = "/home/your_username/.batchtools.slurm.singularity.tmpl",
-          resources=list(ncpus=1, memory='200m', walltime=600, partition='short') # use short instead of quick
-    )
-  ))
-} else {
-  plan(multisession, workers = detectCores() - 1)
-}
+# Always use the Tardis plan
+plan(list(
+  tweak(batchtools_slurm,
+        template = "/home/rstudio/.batchtools.slurm.singularity.tmpl",
+        resources=list(ncpus=1, memory='200m', walltime=600, partition='short')
+  )
+))
 
 # Generate seeds
 generate_seeds <- function(n, seed) {
@@ -40,7 +30,7 @@ generate_seeds <- function(n, seed) {
 }
 
 # Directory to save results
-results_dir <- if (is_on_tardis()) "/path/to/cluster/results" else "results"
+results_dir <- "/path/to/cluster/results"
 
 # Ensure the results directory exists
 if (!dir.exists(results_dir)) {
@@ -51,5 +41,3 @@ if (!dir.exists(results_dir)) {
 save_results <- function(results, filename) {
   saveRDS(results, file.path(results_dir, filename))
 }
-
-
