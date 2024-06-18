f09968284186f4cb5120164a1081904f1e1d4914
lkosanke <kosankle@student.hu-berlin.de>
2024-06-18 15:48:40 +0200

    Added study 4 succesfully, can try study 1 as well if add phi <- 0.6?

--- LK/postprocess_results.R
+++ LK/postprocess_results.R
@@ -1,9 +1,9 @@
-
 # Study 1b
 
 #Load raw results
 
 results_df_raw <- readRDS("LK/SimulationResults/sim1b_results_raw.rds")
+
 # Extract results
 
 extract_results <- function(results_df_raw) {
@@ -32,7 +32,6 @@ extract_results <- function(results_df_raw) {
   return(metrics_list)
 }
 
-metrics_list <- extract_results(results_df_raw)
 
 # Report Bias
 
@@ -94,6 +93,7 @@ report_bias <- function(metrics_list) {
   results_by_n_estimator
 }
 
+
 #Apply changed function
 metrics_list <- extract_results(results_df_raw)
 saveRDS(metrics_list, file = "LK/SimulationResultsProcessed/sim1b_metrics_list.rds")
@@ -101,3 +101,93 @@ saveRDS(metrics_list, file = "LK/SimulationResultsProcessed/sim1b_metrics_list.r
 ### Report Bias
 bias_ci <- suppressMessages(report_bias(metrics_list)) #get rid of annoying "new names" messages
 saveRDS(bias_ci, file = "LK/SimulationResultsProcessed/sim1b_abs_bias_ci.rds")
+
+# Study 4
+
+## Load raw results
+
+results_df_raw <- readRDS("LK/SimulationResults/sim4_results_raw.rds")
+
+# Extract results
+phi <- 0.1
+
+extract_results <- function(results_df_raw){
+  
+  results_raw_combined <- results_df_raw %>%
+    group_by(N, DGM) %>%
+    summarise(across(c(SEM_ML, SEM_ULS, LSAM_ML), 
+                     ~list(unlist(.))), 
+              .groups = "drop")
+  
+  #Compute performance measures
+  results_metrics <- function(values) {
+    list(
+      abs_bias = mean(abs(values - phi)),
+      rel_bias = mean(values - phi) / phi,
+      rmse = sqrt(mean((values - phi)^2)),
+      se_bias = sd(values - phi) / sqrt(length(values)),
+      ci_lower = mean(values - phi) - qt(0.975, df = length(values) - 1) * sd(values - phi) / sqrt(length(values)),
+      ci_upper = mean(values - phi) + qt(0.975, df = length(values) - 1) * sd(values - phi) / sqrt(length(values))
+    )
+  }
+  
+  metrics_list <- results_raw_combined %>%
+    mutate(across(c(SEM_ML, SEM_ULS, LSAM_ML), ~map(.x, results_metrics), .names = "{.col}_metrics")) %>%
+    select(-c(SEM_ML, SEM_ULS, LSAM_ML))  # Drop the original estimator columns
+  
+  return(metrics_list)
+}
+
+
+
+
+# Report Bias
+
+report_bias <- function(metrics_list) {
+  # Define the list of estimators
+  estimators <- c("SEM_ML_metrics", "SEM_ULS_metrics", "LSAM_ML_metrics")
+  
+  # Ensure DGM values are uniquely identified
+  unique_dgms <- unique(metrics_list$DGM)
+  
+  # Process each DGM
+  results_by_dgm <- map(set_names(unique_dgms), ~{
+    # Filter data for the current DGM
+    filtered_data <- metrics_list %>% filter(DGM == .x)
+    
+    # Create a dataframe for each estimator and each N
+    estimator_results <- map_dfr(estimators, ~{
+      estimator_data <- filtered_data[[.x]]
+      
+      # Map each N to create formatted bias strings
+      n_results <- map_dfc(set_names(unique(filtered_data$N)), ~{
+        metrics <- estimator_data[[which(filtered_data$N == .x)]]
+        formatted_bias <- sprintf("%.3f [%.3f, %.3f]", metrics$abs_bias, metrics$ci_lower, metrics$ci_upper)
+        set_names(formatted_bias, paste("N", .x, sep = "_"))
+      })
+      
+      # Add estimator names as the first column
+      tibble(Estimator = sub("_metrics", "", .x)) %>% bind_cols(n_results)
+    })
+    
+    # Name the dataframe with the current N value
+    rename(estimator_results, set_names(names(estimator_results[-1]),paste("N", unique(filtered_data$N), sep = "_")))
+  }, .options = furrr_options(seed = TRUE))  # Ensure reproducibility with seeds
+  
+  # Return a list of dataframes, one for each DGM
+  set_names(results_by_dgm, paste("DGM", unique(metrics_list$DGM), sep = "_"))
+}
+
+
+
+
+#Apply changed function
+metrics_list <- extract_results(results_df_raw)
+saveRDS(metrics_list, file = "LK/SimulationResultsProcessed/sim4_metrics_list.rds")
+
+### Report Bias
+bias_ci <- suppressMessages(report_bias(metrics_list)) #get rid of annoying "new names" messages
+saveRDS(bias_ci, file = "LK/SimulationResultsProcessed/sim4_abs_bias_ci.rds")
+
+
+
