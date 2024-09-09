7664be6cfd30f9702a3c515f8f1f74d86f0c60aa
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2024-09-09 15:08:39 +0200

    tikz as svg for presentation

---
+++ VK/thesis/figures/convert_to_svg.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+pdf_file="convert_tikz_to_svg.pdf"
+model_names=(
+    "model1_1"
+    "model1_2"
+    "model1_3"
+    "model1_4"
+    "model2_1"
+    "model2_2"
+    "model3_1"
+    "model3_2"
+)
+
+for i in "${!model_names[@]}"; do
+    page_num=$((i + 1))
+    output_file="${model_names[$i]}.svg"
+    pdf2svg "$pdf_file" "$output_file" "$page_num"
+    echo "Converted page $page_num to $output_file"
+done
